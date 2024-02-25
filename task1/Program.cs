f48c2bcb5c3c71c92d9a51ff9ee92d11d89a3ffa
Zakhar <info@zspirin.ru>
2024-02-25 20:20:39 +0300

    Task1

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        char[,] array = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+        string result = CreateString(array);
+        System.Console.WriteLine(result);
+        
+    }
+    public static string CreateString(char [,] array) 
+    {
+        string result = "";
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                result += array[i, j];
+            }
+        
+            
+        }    
+        
+        return result;
+    }
+
+}
