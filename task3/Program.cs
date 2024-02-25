83cd8eee3bb7dfc5fbdec5c7466a8dd64c142884
Zakhar <info@zspirin.ru>
2024-02-25 21:07:46 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Task3
+{
+public static void Main(string[] args)
+{
+
+string input = "потоп";
+bool isPalindrome = IsPalindrome(input);
+
+
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+}
+    public static bool IsPalindrome(string str)
+    {
+
+    string normalized = new
+    string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+
+    return normalized.SequenceEqual(normalized.Reverse());
+    }
+}
+
+
