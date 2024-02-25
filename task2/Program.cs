4297cc8e3b1c3f1f4732b427176c3d5534e49ac2
Zakhar <info@zspirin.ru>
2024-02-25 20:28:53 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    public static void Main (string[] args)
+    {
+        
+        string input = "QWERTYUIOPPASJCN43jbdf";
+        
+        string result = input.ToLower();
+
+        System.Console.WriteLine(result);
+
+    
+    }
+
+
+}
